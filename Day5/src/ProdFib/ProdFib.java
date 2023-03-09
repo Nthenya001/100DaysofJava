e5f912f6ab3ba7fa731a388069ef952fae1e7846
Faith Jacob <106926179+Nthenya001@users.noreply.github.com>
2023-03-09 16:33:06 +0300

    Update ProdFib.java

--- Day5/src/ProdFib/ProdFib.java
+++ Day5/src/ProdFib/ProdFib.java
@@ -17,4 +17,4 @@ public class ProdFib {
 // We start with two variables prev and curr initialized to the first two Fibonacci numbers 0 and 1. We keep computing the next Fibonacci 
 // number by adding the previous two (prev and curr) until the product of the current and next Fibonacci numbers (prev * curr) is greater than or equal to the input prod. 
 // If the product is equal to prod, we return an array with prev, curr, and 1 indicating that we found two consecutive Fibonacci numbers whose product is equal to prod. 
-//		Otherwise, we return an array with prev, curr, and 0 indicating that we did not find two consecutive Fibonacci numbers whose product is equal to prod.
+// Otherwise, we return an array with prev, curr, and 0 indicating that we did not find two consecutive Fibonacci numbers whose product is equal to prod.
